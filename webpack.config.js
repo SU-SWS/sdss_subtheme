20fc716bdef07847ca15cfd7dd160155e3355f01
Jen Breese <jbreese@stanford.edu>
2022-06-01 10:48:35 -0700

    EARTH-1646: Updated all references to Starter (#3)
    * EARTH-1646: Updated all references to Starter

--- webpack.config.js
+++ webpack.config.js
@@ -43,12 +43,12 @@ const distAssets =  path.resolve(distDir, 'assets');
 // Start configuring webpack.
 var webpackConfig = {
   // What am i?
-  name: 'stanford_starter',
+  name: 'sdss_subtheme',
   // Allows for map files.
   devtool: 'source-map',
   // What build?
   entry: {
-    "stanford_starter":  path.resolve(srcSass, "config/index.scss"),
+    "sdss_subtheme":  path.resolve(srcSass, "config/index.scss"),
     "scripts":       path.resolve(srcJS,   "scripts.js"),
   },
   // Where put build?
